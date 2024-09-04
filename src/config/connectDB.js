61be3356e248792993371cee9bc6e31ee97f2b9e
trieu <dinh.qtrieu10@gmail.com>
2024-09-04 16:10:15 +0700

    trieu

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -11,7 +11,18 @@ const { Sequelize } = require('sequelize');
 // });
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('sql_user', 'root', '123456', {
   host: 'localhost',
   dialect:  'mysql'
 });
+
+let connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+module.exports = connectDB;
---
+++ src/model/user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { Sequelize } = require("sequelize");
+const {Model} = require(Sequelize);
+module.exports = (Sequelize, DataTypes) => {
+    class User extends Model {
+        static associate(models) {
+            //Define association here
+        }
+    };
+    User.init({
+        id: DataTypes.INTEGER,
+        firstName: DataTypes.STRING,
+        lastName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        address: DataTypes.STRING,
+        gender: DataTypes.BOOLEAN,
+        roleid: DataTypes.STRING
+    }, {
+        Sequelize,
+        modelName: 'user',
+    });
+    return User;
+};
